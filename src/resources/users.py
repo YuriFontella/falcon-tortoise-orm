b03abe592b99e3bd3b61ec3448ac5988f6a9724b
Yuri Fontella <yurifc4@gmail.com>
2024-02-01 12:16:51 -0300

    limiter

--- src/resources/users.py
+++ src/resources/users.py
@@ -1,6 +1,8 @@
 from src.models.source import Users
+from src.storage.limits import limiter
 
 class UsersResource:
+    @limiter.limit()
     async def on_get(self, req, resp):
         user = await Users.first().values('name')
         resp.media = user
