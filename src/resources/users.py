d652a7cbb3b69e25929eb05ae8c4db4244a65b0a
Yuri Fontella <yurifc4@gmail.com>
2024-02-01 21:36:01 -0300

    remove limiter

--- src/resources/users.py
+++ src/resources/users.py
@@ -1,8 +1,6 @@
 from src.models.main import Users
-from src.storage.limits import limiter
 
 class UsersResource:
-    @limiter.limit()
     async def on_get(self, req, resp):
         user = await Users.first().values('name')
         resp.media = user
